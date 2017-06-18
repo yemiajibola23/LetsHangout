7f5a9af437c72a11c29c4454ca11a56ea17605ab
Yemi Ajibola <yemiajibola@Yemis-MacBook-Pro.local>
2017-06-17 22:49:07 -0400

    Added test suite for LoginViewModel tests but need to figure out how to make AuthenticationState equatable.

---
+++ LetsHangoutTests/LoginViewModelTests.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewModelTests.swift
+//  LetsHangout
+//
+//  Created by Yemi Ajibola on 6/17/17.
+//  Copyright © 2017 Yemi Ajibola. All rights reserved.
+//
+
+import XCTest
+@testable import LetsHangout
+
+class LoginViewModelTests: XCTestCase {
+    
+    var loginViewModel: LoginViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        loginViewModel = LoginViewModel()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+//    func testLoginViewModelStateAfterInitialization() {
+//       XCTAssertTrue(loginViewModel.state == AuthenticationState.notAuthenticated)
+//    }
+    
+}
